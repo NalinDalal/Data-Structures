85d5a81b277fde9e9e8d62ceea94fb5b1f0905af
NalinDalal <nalindalal2004@gmail.com>
2023-12-05 09:43:29 +0530

    made the first one for inserting and printing a linked list

---
+++ 2.Linked_list/1.linked_list.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+using namespace std;
+
+class node{
+    public:
+    int data;
+    node* next;
+
+    node(int val){
+        data=val;
+        next=NULL;
+    }
+};
+
+//Insertion
+void insertAthead(node* &head, int val){
+    node* n=new node(val);
+    n->next=head;
+    head=n;
+}
+
+void insertAtTail(node* head, int val){
+    node* n=new node(val);  //node n ke data field me val
+
+    if(head==NULL){
+        head=n; //n is first node
+        return;
+    }
+
+    node* temp=head;    //iterator
+    while(temp->next!=NULL){
+        temp=temp->next;
+    }
+    temp->next=n;
+}
+
+//printing
+void display(node* head){
+    node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<"->";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+
+//searching
+bool search(node* head, int key){
+    node* temp=head;
+    while(temp!=NULL){
+        if(temp->data==key){
+            return true;
+        }
+        return false;
+    }
+}
+
+int main(){
+
+    //to run it
+
+    node* head=NULL;
+    insertAtTail(head,1);
+    insertAtTail(head,2);
+    insertAtTail(head,3);
+    display(head);
+    insertAthead(head,4);
+    display(head);
+    cout<<search(head,5)<<endl;
+
+    cin.get();
+}
