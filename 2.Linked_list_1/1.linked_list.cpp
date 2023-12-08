4eb9ca2a9d4beff7431ab310cd8757619c7552d3
NalinDalal <nalindalal2004@gmail.com>
2023-12-08 19:01:31 +0530

    we just made all files on single linked and it's algorithms

--- 2.Linked_list_1/1.linked_list.cpp
+++ 2.Linked_list_1/1.linked_list.cpp
@@ -41,7 +41,7 @@ void display(node* head){
         cout<<temp->data<<"->";
         temp=temp->next;
     }
-    cout<<endl;
+    cout<<"NULL"<<endl;
 }
 
 //searching
@@ -51,9 +51,96 @@ bool search(node* head, int key){
         if(temp->data==key){
             return true;
         }
-        return false;
+        temp=temp->next;    //moves the pointer otherwise the loop will run forever
     }
+    return false;
 }
+
+//reverse linked list
+node* reverse(node* &head){
+    node* prevptr=NULL;
+    node* currptr=head;
+    node* nextptr;
+
+    while(cerr!=NULL){
+        nextptr=currptr->next;
+        currptr->next=prevptr;
+
+        prevptr=currptr;
+        currptr=nextptr;
+
+    }
+
+    return prevptr;
+}
+
+/*
+head->1->2->3->4->NULL
+
+head->4->3->2->1->NULL
+
+2 Methods: Iterative and Recursive
+
+Iterative:
+3 Pointers:previous,current,next
+    current at head
+    previous at null
+    next at current->next=previous
+
+    update head->link=previous
+
+    increase pointers
+    same procedure
+
+    for last node=current
+    current->next=previous
+
+    break loop at current=NULL
+
+    update head thru previous
+
+    complexity:O(n)
+*/
+node *reverseRecursive(node* &head){
+
+    if(head==NULL||head->next==NULL){
+        return head;
+    }
+
+    node* newhead=reverseRecursive(head->next);
+    head->next->next=head;
+    head->next=NULL;
+
+    return newhead;
+}
+
+/////////////////////////////////////////////////////////////////////
+/*
+reverse k nodes of linked list like k=2 :first 2 then next 2 then next 2
+time complexity:O(n)
+*/
+
+node* reversek(node* &head,int k){
+    node* prevptr=NULL;
+    node* currptr=head;
+    node* nextptr;
+
+    int count=0;
+    while(currptr!=NULL && count<k){
+        nextptr=currptr->next;
+        currptr->next=prevptr;
+        prevptr=currptr;
+        currptr=nextptr;
+        count++;
+    }
+
+    //recursive call for rest of linked list
+    if(nextptr!=NULL){
+        head->next=reversek(nextptr,k);
+    }
+    head->next=reversek(nextptr,k);
+}
+
 
 int main(){
 
@@ -66,7 +153,20 @@ int main(){
     display(head);
     insertAthead(head,4);
     display(head);
-    cout<<search(head,5)<<endl;
+    //--------------------
+    node* newhead=reverse(head);
+    node* newhead=reverseRecursive(head->next);
+    display(newhead);
+    //--------------------
+    cout<<search(head,5)<<endl; //output=0
+    cout<<search(head,3)<<endl;
+
+    //well it is done
+
+    //reverse k nodes of linked list
+    int k=2;
+    node* newHead=reversek(head,k);
+    display(newhead);
 
     cin.get();
 }
