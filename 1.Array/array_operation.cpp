af54f7a692396ec05e70efe58a27626333cece5c
Nalin <nalindalal2004@gmail.com>
2025-04-29 11:17:13 +0530

    removed bin

--- 1.Array/array_operation.cpp
+++ 1.Array/array_operation.cpp
@@ -1,26 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int n;
-    int arr[n];
-    cout<<"Enter no of elements in array:";
-    cin>>n;
-    // cout<<"Elements are:";
-    // int arr[n];
-    // for(i=0;i<=n-1;i++){
-    //     cin>>arr[i];
-    // }
+int main() {
+  int n;
+  int arr[n];
+  cout << "Enter no of elements in array:";
+  cin >> n;
+  // cout<<"Elements are:";
+  // int arr[n];
+  // for(i=0;i<=n-1;i++){
+  //     cin>>arr[i];
+  // }
 
-    // cout<<arr[n]<<endl;
+  // cout<<arr[n]<<endl;
 
-    for(i=0;i<n;i++){
-        cin>>arr[i];
-    }
+  for (int i = 0; i < n; i++) {
+    cin >> arr[i];
+  }
 
-    for(i=0;i<n;i++){
-        cout<<arr[i]<<" ";
-    }
+  for (int i = 0; i < n; i++) {
+    cout << arr[i] << " ";
+  }
 
-    cin.get();
+  cin.get();
 }
